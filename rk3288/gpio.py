9a3ea98b10a0ed1726d644ef6087bc2201758e4a
zhansb <service@t-firefly.com>
2016-06-10 16:56:52 +0800

    rk3288: support for setting gpio iomux

--- rk3288/gpio.py
+++ rk3288/gpio.py
@@ -8,53 +8,66 @@ import logging
 
 
 class MapReg:
-    def __init__(self, addr, size):
+    def __init__(self, name, addr, size):
+        self.name = name # just for debug
         self.addr = addr
         self.size = size
         self.mem = devmem.DevMem(addr, size, "/dev/mem", 0)
+
     def write(self, offset, val):
         logging.debug("write: \toffset=%#06x, val=%#010x\t(%s)" % (offset, val, bin(val)))
         self.mem.write(offset, [val,])
-        pass
 
     def read(self, offset):
         val = 0xac00ac01
         val = self.mem.read(offset, 1)[0]
         logging.debug("read: \toffset=%#06x, val=%#010x\t(%s)" % (offset, val, bin(val)))
         return val
-        pass
+
+    def __str__(self):
+        return 'MapReg: name=%s, phyaddr=%#010x, len=%#x' % (self.name, self.addr, self.size)
+    __repr__ = __str__
 
 class Bank:
-    def __init__(self, base, iomux, pull, drv):
-        self.base   = base
+    def __init__(self, ctrl, iomux, pull, drv):
+        self.ctrl   = ctrl
         self.iomux  = iomux
         self.pull   = pull
         self.drv    = drv
 
+    def __str__(self):
+        return 'ctrl\t%s\niomux\t%s\npull\t%s\ndrv\t%s' % (self.ctrl, self.iomux, self.pull, self.drv)
+    __repr__ = __str__
+
 class Gpio:
     def __init__(self):
-        self.gpio0_base =   MapReg(0xff750000,0x100)
-        self.gpio0_iomux =  MapReg(0xff730084,0x0c)
-        self.gpio0_pull =   MapReg(0xff730064,0x0c)
-        self.gpio0_drv =    MapReg(0xff730070,0x0c)
+        self._set_mapreg('gpio0_ctrl',0xff750000,0x100)
+        self._set_mapreg('gpio0_iomux',0xff730084,0x0c)
+        self._set_mapreg('gpio0_pull',0xff730064,0x0c)
+        self._set_mapreg('gpio0_drv',0xff730070,0x0c)
+        self._set_mapreg('gpio18_iomux',0xff770000,0x140)
+        self._set_mapreg('gpio18_pull',0xff770140,0x80)
+        self._set_mapreg('gpio18_drv',0xff7701c0,0x80)
+        self._set_mapreg('gpio1_ctrl',0xff780000,0x100)
+        self._set_mapreg('gpio2_ctrl',0xff790000,0x100)
+        self._set_mapreg('gpio3_ctrl',0xff7a0000,0x100)
+        self._set_mapreg('gpio4_ctrl',0xff7b0000,0x100)
+        self._set_mapreg('gpio5_ctrl',0xff7c0000,0x100)
+        self._set_mapreg('gpio6_ctrl',0xff7d0000,0x100)
+        self._set_mapreg('gpio7_ctrl',0xff7e0000,0x100)
+        self._set_mapreg('gpio8_ctrl',0xff7f0000,0x100)
+        self._set_mapreg('gpio15_ctrl',0xff7f2000,0x100)
 
-        self.gpio18_iomux = MapReg(0xff770000,0x140)
-        self.gpio18_pull =  MapReg(0xff770140,0x80)
-        self.gpio18_drv =   MapReg(0xff7701c0,0x80)
-        
-        self.gpio1_base =   MapReg(0xff780000,0x100)
-        self.gpio2_base =   MapReg(0xff790000,0x100)
-        self.gpio3_base =   MapReg(0xff7a0000,0x100)
-        self.gpio4_base =   MapReg(0xff7b0000,0x100)
-        self.gpio5_base =   MapReg(0xff7c0000,0x100)
-        self.gpio6_base =   MapReg(0xff7d0000,0x100)
-        self.gpio7_base =   MapReg(0xff7e0000,0x100)
-        self.gpio8_base =   MapReg(0xff7f0000,0x100)
-        self.gpio15_base =  MapReg(0xff7f2000,0x100)
         self.regs= {
-            "GPIO0": Bank(self.gpio0_base, self.gpio0_iomux, self.gpio0_pull, self.gpio0_drv),
-            "GPIO8": Bank(self.gpio8_base, self.gpio18_iomux, self.gpio18_pull, self.gpio18_drv),
+            "GPIO0": Bank(self.gpio0_ctrl, self.gpio0_iomux, self.gpio0_pull, self.gpio0_drv),
+            "GPIO7": Bank(self.gpio7_ctrl, self.gpio18_iomux, self.gpio18_pull, self.gpio18_drv),
+            "GPIO8": Bank(self.gpio8_ctrl, self.gpio18_iomux, self.gpio18_pull, self.gpio18_drv),
         }
+
+    def _set_mapreg(self, mapname, addr, size):
+        mr=MapReg(mapname, addr, size)
+        setattr(self, mapname, mr)
+
     def _get_bank_pin(self, gpio):
         bank = gpio[:5]
         pin = PIN[gpio[5:]]
@@ -75,11 +88,11 @@ class Gpio:
             logging.error("set_dir: set <%s>=%d error!" % (gpio, dir))
             return None
 
-        reg = regs.base
-        val = reg.read(BaseReg.GPIO_SWPORT_DDR)
+        reg = regs.ctrl
+        val = reg.read(CtrlReg.GPIO_SWPORT_DDR)
         val &= (~(1<<pin))
         val |= (dir<<pin)
-        reg.write(BaseReg.GPIO_SWPORT_DDR, val)
+        reg.write(CtrlReg.GPIO_SWPORT_DDR, val)
 
     def get_level(self, gpio):
         """
@@ -91,8 +104,8 @@ class Gpio:
         except:
             logging.error("get_level: get <%s> error!" % (gpio))
             return None
-        reg = regs.base
-        val = reg.read(BaseReg.GPIO_EXT_PORT)
+        reg = regs.ctrl
+        val = reg.read(CtrlReg.GPIO_EXT_PORT)
         val >>=pin
         val &=1
         logging.debug("get_level: <%s>=%d" % (gpio, val))
@@ -111,18 +124,77 @@ class Gpio:
         except:
             logging.error("set_level: set <%s>=%d error!" % (gpio, level))
             return None
-        reg = regs.base
-        val = reg.read(BaseReg.GPIO_SWPORT_DR)
-        reg.write(BaseReg.GPIO_SWPORT_DR, (val & (~(1<<pin))) | (level<<pin))
+        reg = regs.ctrl
+        val = reg.read(CtrlReg.GPIO_SWPORT_DR)
+        reg.write(CtrlReg.GPIO_SWPORT_DR, (val & (~(1<<pin))) | (level<<pin))
 
     def set_mux(self, gpio, mux):
-        pass
+        """
+        set GPIO mux
+        :mux: refer to 
+        """
+        logging.debug("set_mux: set <%s>=%d" % (gpio, mux))
+        try:
+            offset,bits = get_mux_offset_bits(gpio)
+        except AttributeError as e:
+            logging.error("set_mux: get <%s> offset error" % (gpio))
+            return None
+
+        try:
+            bank,pin,regs = self._get_bank_pin(gpio)
+        except:
+            logging.error("set_rk32_iomux: set <%s>=%d error" % (gpio, mux))
+            return None
+
+        set_rk32_iomux(bank, pin, regs.iomux, offset, bits, mux)
 
     def set_pull(self, gpio, pd):
         pass
 
     def set_drv(self, gpio, drv):
         pass
+
+def get_mux_offset_bits(gpio):
+    offset = -1
+    bits = 0
+    try:
+        offset = getattr(MuxReg, "GRF_" + gpio[:6] + "_IOMUX")
+        bits = 2
+    except AttributeError as e:
+        if gpio[6] < 4:
+            offset = getattr(MuxReg, "GRF_" + gpio[:6] + "L_IOMUX")
+        else:
+            offset = getattr(MuxReg, "GRF_" + gpio[:6] + "H_IOMUX")
+        bits=4
+    return offset,bits
+
+def set_rk32_iomux(bank, pin, reg, offset, bits, mux):
+    if bits == 2:
+        bit = (pin % 8) * 2
+
+        if bank == "GPIO0":
+            data = reg.read(offset);
+            data &= ~(3<<bit);
+            data |= (mux & 3) << bit;
+            reg.write(offset, data);
+        else:
+            data = (3 << (bit + 16));
+            data |= (mux & 3) << bit;
+            reg.write(offset, data);
+    elif bits == 4:
+        bit = (pin % 4) * 4;
+
+        if bank == "GPIO0":
+            data = reg.read(offset);
+            data &= ~(0x0f<<bit);
+            data |= (mux & 0x0f) << bit;
+            reg.write(offset, data);
+        else:
+            data = (0x0f << (bit + 16));
+            data |= (mux & 0x0f) << bit;
+            reg.write(offset, data);
+    else:
+        logging.error("set_mux: set <%s-%s> mux<%s> error" % (bank, pin, mux))
 
 def gpio_init():
     return gpio()
--- test.py
+++ test.py
@@ -10,8 +10,15 @@ logging.basicConfig(level=logging.DEBUG)
 LED_BLUE="GPIO8A1"
 LED_YELLOW="GPIO8A2"
 
+PWM1="GPIO7A1"
+GPIO7A1_IOMUX_GPIO=0
+GPIO7A1_IOMUX_PWM1=1
+
+
 if __name__ ==  '__main__':
     gpio = Gpio()
+    gpio.set_mux(PWM1, GPIO7A1_IOMUX_PWM1)
+
     gpio.set_dir(LED_BLUE, GpioDir.OUTPUT)
     gpio.set_dir(LED_YELLOW, GpioDir.OUTPUT)
     while(True):
@@ -23,4 +30,4 @@ if __name__ ==  '__main__':
         gpio.set_level(LED_YELLOW, GpioLevel.HIGH)
         gpio.get_level(LED_YELLOW)
         time.sleep(0.5)
-    #gpio.set_level("GPIO7C5", GpioLevel.LOW)
+    1#gpio.set_level("GPIO7C5", GpioLevel.LOW)
