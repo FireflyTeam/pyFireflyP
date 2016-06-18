e9742869320f22f341f85977857042d4d8c44c4e
zhansb <service@t-firefly.com>
2016-06-18 11:23:02 +0800

    rk3288: modify the interface of Gpio

--- gpio_test.py
+++ gpio_test.py
@@ -3,12 +3,9 @@
 from rk3288.gpio import Gpio
 from rk3288.cons import *
 
-import unittest
 import time
-import logging
 import pdb
 
-logging.basicConfig(level=logging.DEBUG)
 
 LED_BLUE="GPIO8A1"
 LED_YELLOW="GPIO8A2"
@@ -19,51 +16,28 @@ GPIO7A1_IOMUX_PWM1=1
 
 
 if __name__ ==  '__main__':
-    gpio = Gpio()
+    #pdb.set_trace()
+    Gpio.init()
 
-    G0='GPIO0B5'
-    G5='GPIO5B1'
+    g0b5=Gpio('GPIO0B5')
+    g5b1=Gpio('GPIO5B1')
+    # except test for error gpio
+    # Gpio('GPIO9A15').set_mux(0)	
 
-    gpio.set_dir(G5, GpioDir.OUTPUT)
-    pdb.set_trace()
-    gpio.set_drv(G5, GpioDrv.DRV_2MA)
-    gpio.set_drv(G5, GpioDrv.DRV_12MA)
+    g0b5.set_dir(GpioDir.OUTPUT)
+    g0b5.set_drv(GpioDrv.DRV_2MA)
+    g0b5.set_drv(GpioDrv.DRV_12MA)
 
 
-    gpio.set_mux(PWM1, GPIO7A1_IOMUX_PWM1)
-
-    gpio.set_dir(LED_BLUE, GpioDir.OUTPUT)
-    gpio.set_dir(LED_YELLOW, GpioDir.OUTPUT)
+    gpio_blue_led=Gpio(LED_BLUE)
+    gpio_yellow_led=Gpio(LED_YELLOW)
+    gpio_blue_led.set_dir(GpioDir.OUTPUT)
+    gpio_yellow_led.set_dir(GpioDir.OUTPUT)
     while(True):
-        gpio.set_level(LED_BLUE, GpioLevel.HIGH)
-        gpio.set_level(LED_YELLOW, GpioLevel.LOW)
-        gpio.get_level(LED_YELLOW)
+        gpio_blue_led.set_level(GpioLevel.HIGH)
+        gpio_yellow_led.set_level(GpioLevel.LOW)
         time.sleep(0.5)
-        gpio.set_level(LED_BLUE, GpioLevel.LOW)
-        gpio.set_level(LED_YELLOW, GpioLevel.HIGH)
-        gpio.get_level(LED_YELLOW)
+        gpio_blue_led.set_level(GpioLevel.LOW)
+        gpio_yellow_led.set_level(GpioLevel.HIGH)
         time.sleep(0.5)
-    #gpio.set_level("GPIO7C5", GpioLevel.LOW)
-    #gpio.set_mux('GPIO9A15', 0)	# except test for error gpio
 
-class TestGpio(unittest.TestCase):
-    '''
-    run the follow cmd to auto test:
-    python -m unittest gpio_test
-    '''
-    def test_pull(self):
-        '''
-        Make sure GPIO5B1(UART1_TX) connect nothing!!
-        Make sure GPIO0B5 connect nothing!!
-        '''
-        def _test_pull(gpio, pin):
-            gpio.set_dir(pin, GpioDir.INPUT)
-
-            gpio.set_pull(pin, GpioPull.DOWN)
-            self.assertEqual(gpio.get_level(pin), GpioLevel.LOW)
-            gpio.set_pull(pin, GpioPull.UP)
-            self.assertEqual(gpio.get_level(pin), GpioLevel.HIGH)
-
-        gpio=Gpio()
-        _test_pull(gpio, 'GPIO5B1')
-        _test_pull(gpio, 'GPIO0B5')
--- rk3288/gpio.py
+++ rk3288/gpio.py
@@ -35,152 +35,176 @@ class Bank:
         self.drv    = drv
 
     def __str__(self):
-        return 'ctrl\t%s\niomux\t%s\npull\t%s\ndrv\t%s' % (self.ctrl, self.iomux, self.pull, self.drv)
+        return '\nctrl\t%s\niomux\t%s\npull\t%s\ndrv\t%s' % (self.ctrl, self.iomux, self.pull, self.drv)
     __repr__ = __str__
 
 class Gpio:
-    def __init__(self):
-        self._set_mapreg('gpio0_ctrl',0xff750000,0x100)
-       #self._set_mapreg('gpio0_iomux',0xff730084,0x0c)
-       #self._set_mapreg('gpio0_pull',0xff730064,0x0c)
-       #self._set_mapreg('gpio0_drv',0xff730070,0x0c)
-       #self._set_mapreg('gpio18_iomux',0xff770000,0x140)
-       #self._set_mapreg('gpio18_pull',0xff770140,0x80)
-       #self._set_mapreg('gpio18_drv',0xff7701c0,0x80)
-        self._set_mapreg('gpio0_base',0xff730000,0x7c)
-        self._set_mapreg('gpio18_base',0xff770000,0x240)
+    _inited = 0
 
-        self._set_mapreg('gpio1_ctrl',0xff780000,0x100)
-        self._set_mapreg('gpio2_ctrl',0xff790000,0x100)
-        self._set_mapreg('gpio3_ctrl',0xff7a0000,0x100)
-        self._set_mapreg('gpio4_ctrl',0xff7b0000,0x100)
-        self._set_mapreg('gpio5_ctrl',0xff7c0000,0x100)
-        self._set_mapreg('gpio6_ctrl',0xff7d0000,0x100)
-        self._set_mapreg('gpio7_ctrl',0xff7e0000,0x100)
-        self._set_mapreg('gpio8_ctrl',0xff7f0000,0x100)
-        self._set_mapreg('gpio15_ctrl',0xff7f2000,0x100)
-
-        self.regs= {
-            "GPIO0": Bank(self.gpio0_ctrl, self.gpio0_base, self.gpio0_base, self.gpio0_base),
-            "GPIO1": Bank(self.gpio1_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-            "GPIO2": Bank(self.gpio2_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-            "GPIO3": Bank(self.gpio3_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-            "GPIO4": Bank(self.gpio4_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-            "GPIO5": Bank(self.gpio5_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-            "GPIO6": Bank(self.gpio6_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-            "GPIO7": Bank(self.gpio7_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-            "GPIO8": Bank(self.gpio8_ctrl, self.gpio18_base, self.gpio18_base, self.gpio18_base),
-        }
-
-    def _set_mapreg(self, mapname, addr, size):
+    @staticmethod
+    def _set_mapreg(mapname, addr, size):
         mr=MapReg(mapname, addr, size)
-        setattr(self, mapname, mr)
+        setattr(Gpio, '_' + mapname, mr)
 
-    def _get_bank_pin(self, gpio):
+    @staticmethod
+    def init():
+        """
+        Init GPIO function
+        implement it before using Gpio
+        """
+        if Gpio._inited > 0:
+            logging.error("Gpio have inited!")
+            return None
+        Gpio._inited = 1
+
+        Gpio._set_mapreg('gpio0_ctrl',0xff750000,0x100)
+       #Gpio._set_mapreg('gpio0_iomux',0xff730084,0x0c)
+       #Gpio._set_mapreg('gpio0_pull',0xff730064,0x0c)
+       #Gpio._set_mapreg('gpio0_drv',0xff730070,0x0c)
+       #Gpio._set_mapreg('gpio18_iomux',0xff770000,0x140)
+       #Gpio._set_mapreg('gpio18_pull',0xff770140,0x80)
+       #Gpio._set_mapreg('gpio18_drv',0xff7701c0,0x80)
+        Gpio._set_mapreg('gpio0_base',0xff730000,0x7c)
+        Gpio._set_mapreg('gpio18_base',0xff770000,0x240)
+
+        Gpio._set_mapreg('gpio1_ctrl',0xff780000,0x100)
+        Gpio._set_mapreg('gpio2_ctrl',0xff790000,0x100)
+        Gpio._set_mapreg('gpio3_ctrl',0xff7a0000,0x100)
+        Gpio._set_mapreg('gpio4_ctrl',0xff7b0000,0x100)
+        Gpio._set_mapreg('gpio5_ctrl',0xff7c0000,0x100)
+        Gpio._set_mapreg('gpio6_ctrl',0xff7d0000,0x100)
+        Gpio._set_mapreg('gpio7_ctrl',0xff7e0000,0x100)
+        Gpio._set_mapreg('gpio8_ctrl',0xff7f0000,0x100)
+        Gpio._set_mapreg('gpio15_ctrl',0xff7f2000,0x100)
+
+        Gpio._regs= {
+            "GPIO0": 
+                Bank(Gpio._gpio0_ctrl, Gpio._gpio0_base, Gpio._gpio0_base, Gpio._gpio0_base),
+            "GPIO1": 
+                Bank(Gpio._gpio1_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+            "GPIO2": 
+                Bank(Gpio._gpio2_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+            "GPIO3": 
+                Bank(Gpio._gpio3_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+            "GPIO4": 
+                Bank(Gpio._gpio4_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+            "GPIO5": 
+                Bank(Gpio._gpio5_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+            "GPIO6": 
+                Bank(Gpio._gpio6_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+            "GPIO7": 
+                Bank(Gpio._gpio7_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+            "GPIO8": 
+                Bank(Gpio._gpio8_ctrl, Gpio._gpio18_base, Gpio._gpio18_base, Gpio._gpio18_base),
+        }
+    @staticmethod
+    def exit():
+        Gpio._inited = 0
+        pass
+
+    def __init__(self, gpio):
+        self.gpio = gpio
+
         sbank = gpio[:5]
-        bank = BANK[sbank]
-        pin = PIN[gpio[5:]]
-        regs = self.regs[sbank]
-        return bank,pin,regs
+        self.bank = BANK[sbank]
+        self.pin = PIN[gpio[5:]]
+        self._regs = Gpio._regs[sbank]
 
-    def set_dir(self, gpio, dir):
+    def __str__(self):
+        return self.gpio
+
+    def __repr__(self):
+        return '%s: bank=%d, pin=%d\n%s' % (self.gpio, self.bank, self.pin, self._regs)
+
+
+    def set_dir(self, dir):
         """
         set GPIO direction
         :dir: refer to GpioDir
         """
-        logging.debug("set_dir: set <%s>=%d" % (gpio, dir))
+        logging.debug("set_dir: set <%s>=%d" % (self.gpio, dir))
         assert dir in cons_list(GpioDir)
 
-        self.set_mux(gpio, GpioMux.MUX_GPIO)   #set iomux=gpio default
-        bank,pin,regs = self._get_bank_pin(gpio)
+        self.set_mux(GpioMux.MUX_GPIO)   #set iomux=gpio default
 
-        reg = regs.ctrl
+        reg = self._regs.ctrl
         val = reg.read(RegCtrl.GPIO_SWPORT_DDR)
-        val &= (~(1<<pin))
-        val |= (dir<<pin)
+        val &= (~(1<<self.pin))
+        val |= (dir<<self.pin)
         reg.write(RegCtrl.GPIO_SWPORT_DDR, val)
 
-    def get_level(self, gpio):
+    def get_level(self):
         """
         Returns the level of the pin for input direction
         or return setting of the DR register for output gpios.
         """
-        bank,pin,regs = self._get_bank_pin(gpio)
 
-        reg = regs.ctrl
+        reg = self._regs.ctrl
         val = reg.read(RegCtrl.GPIO_EXT_PORT)
-        val >>=pin
+        val >>=self.pin
         val &=1
-        logging.debug("get_level: <%s>=%d" % (gpio, val))
+        logging.debug("get_level: <%s>=%d" % (self.gpio, val))
         return val
 
-    def set_level(self, gpio, level):
+    def set_level(self, level):
         """
         set GPIO output signal
         :level: refer to GpioLevel
         """
-        logging.debug("set_level: set <%s>=%d" % (gpio, level))
+        logging.debug("set_level: set <%s>=%d" % (self.gpio, level))
         assert level in cons_list(GpioLevel)
 
-        bank,pin,regs = self._get_bank_pin(gpio)
-
-        reg = regs.ctrl
+        reg = self._regs.ctrl
         val = reg.read(RegCtrl.GPIO_SWPORT_DR)
-        reg.write(RegCtrl.GPIO_SWPORT_DR, (val & (~(1<<pin))) | (level<<pin))
+        reg.write(RegCtrl.GPIO_SWPORT_DR, (val & (~(1<<self.pin))) | (level<<self.pin))
 
-    def set_mux(self, gpio, mux):
+    def set_mux(self, mux):
         """
         set GPIO mux
         :mux: refer to GpioMux
         """
-        logging.debug("set_mux: set <%s>=%d" % (gpio, mux))
+        logging.debug("set_mux: set <%s>=%d" % (self.gpio, mux))
         assert mux in cons_list(GpioMux)
 
-        bank,pin,regs = self._get_bank_pin(gpio)
-
         try:
-            offset,bits = get_mux_offset_bits(gpio)
+            offset,bits = get_mux_offset_bits(self.gpio)
         except:
-            logging.warn("set_mux: unknow mux of <%s>" % (gpio))
+            logging.warn("set_mux: unknow mux of <%s>" % (self.gpio))
             return None
 
-        set_rk32_iomux(bank, pin, regs.iomux, offset, bits, mux)
+        set_rk32_iomux(self.bank, self.pin, self._regs.iomux, offset, bits, mux)
 
-    def set_pull(self, gpio, pull):
+    def set_pull(self, pull):
         """
         set GPIO pull
         :pull: refer to GpioPull
         """
-        logging.debug("set_pull: set <%s>=%d" % (gpio, pull))
+        logging.debug("set_pull: set <%s>=%d" % (self.gpio, pull))
         assert pull in cons_list(GpioPull)
 
-        bank,pin,regs = self._get_bank_pin(gpio)
-
         try:
-            offset= get_pull_offset_bits(gpio)
+            offset= get_pull_offset_bits(self.gpio)
         except:
-            logging.warn("set_pull: unknow pull of <%s>" % (gpio))
+            logging.warn("set_pull: unknow pull of <%s>" % (self.gpio))
             return None
 
-        set_rk32_pull(pin, regs.pull, offset, pull)
+        set_rk32_pull(self.pin, self._regs.pull, offset, pull)
 
-    def set_drv(self, gpio, drv):
+    def set_drv(self, drv):
         """
         set GPIO drv
         :drv: refer to GpioDrv
         """
-        logging.debug("set_drv: set <%s>=%d" % (gpio, drv))
+        logging.debug("set_drv: set <%s>=%d" % (self.gpio, drv))
         assert drv in cons_list(GpioDrv)
 
-        bank,pin,regs = self._get_bank_pin(gpio)
-
         try:
-            offset= get_drv_offset_bits(gpio)
+            offset= get_drv_offset_bits(self.gpio)
         except:
-            logging.warn("set_drv: unknow drv of <%s>" % (gpio))
+            logging.warn("set_drv: unknow drv of <%s>" % (self.gpio))
             return None
 
-        set_rk32_drv(pin, regs.drv, offset, drv)
+        set_rk32_drv(self.pin, self._regs.drv, offset, drv)
 
 
 def get_pull_offset_bits(gpio):
@@ -245,6 +269,36 @@ def gpio_init():
 if __name__ ==  '__main__':
     gpio_init()
 
+import unittest
+import logging
 
+class TestGpio(unittest.TestCase):
+    '''
+    run the follow cmd to auto test:
+    python -m unittest rk3288.gpio
+    '''
+    def test_pull(self):
+        '''
+        Make sure GPIO5B1(UART1_TX) connect nothing!!
+        Make sure GPIO0B5 connect nothing!!
+        '''
+        def _test_pull(ioname):
+            gpio=Gpio(ioname)
+            gpio.set_dir(GpioDir.INPUT)
 
+            gpio.set_pull(GpioPull.DOWN)
+            self.assertEqual(gpio.get_level(), GpioLevel.LOW)
+            gpio.set_pull(GpioPull.UP)
+            self.assertEqual(gpio.get_level(), GpioLevel.HIGH)
+
+        _test_pull('GPIO5B1')
+        _test_pull('GPIO0B5')
+
+    def setUp(self):
+        logging.basicConfig(level=logging.DEBUG)
+        Gpio.init()
+        logging.debug("setup gpio")
+
+    def tearDown(self):
+        pass
 
